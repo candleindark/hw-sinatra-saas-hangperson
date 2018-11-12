d2d99b934bab88f86f21ca67e8cb5b7daabc1b87
Isaac To <candleindark@gmail.com>
2018-11-12 15:58:19 -0800

    Implement HangpersonGame#guess

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,6 +10,29 @@ class HangpersonGame
   attr_reader :guesses
   attr_reader :wrong_guesses
 
+  # To process a guessed letter
+  def guess(letter)
+    unless (letter.is_a? String) and (letter.match /^[[:alpha:]]{1}$/)
+      # The argument is not a letter
+      raise ArgumentError, 'The argument is not a letter.'
+    end
+
+    # Add support for case insensitivity
+    letter = letter.downcase
+    if (@guesses.include? letter) or (@wrong_guesses.include? letter)
+      # The letter is a repeated guess
+      return false
+    end
+
+    if @word.include? letter
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+
+    true
+  end
+
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
