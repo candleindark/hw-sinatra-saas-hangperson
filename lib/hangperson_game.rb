ba1e50f97e8537bad3726464dd2ba402a710a227
Isaac To <candleindark@gmail.com>
2018-11-11 21:38:50 -0800

    Implement initializer of HangpersonGame

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,16 +1,14 @@
 class HangpersonGame
-
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
+  # For representing a hang person game
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  attr_reader :word
+  attr_reader :guesses
+  attr_reader :wrong_guesses
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
